b46ec3330b3eb6e3ea61ead307af9d9b853f17f3
lhikarul <711hikaru@gmail.com>
2024-06-13 11:30:16 +0800

    chore: css 遷移

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,28 @@ module.exports = {
   mode: "development",
   entry: "/src/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.([png|svg|jpg|jpeg|gif])$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024,
+          },
+        },
+        generator: {
+          filename: "images/[name].[hash:6][ext]",
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
